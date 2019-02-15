262aa824b66b843fa61c350edca7108824f9c263
Joanna Gammel <jgammel@Joannas-MacBook-Pro.local>
2019-02-15 16:37:15 -0500

    remove archive title

--- themes/drw/functions.php
+++ themes/drw/functions.php
@@ -575,3 +575,25 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Remove archive label
+ */
+ 
+ function drw_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    }
+  
+    return $title;
+}
+ 
+add_filter( 'get_the_archive_title', 'drw_archive_title' );
+
