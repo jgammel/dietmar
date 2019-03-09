0c877249a1ff835f2916e13716dbb70d86629263
Joanna Gammel <jgammel@risd.edu>
2019-03-09 17:22:09 -0500

    visually hidden prev/next buttons for In Words

--- themes/drw/single-post.php
+++ themes/drw/single-post.php
@@ -17,8 +17,12 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content-single-post', get_post_type() );
 
-			the_post_navigation();
-
+			?>
+			<div class="visually-hidden">
+			<?php the_post_navigation();?>
+			</div>
+			
+			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
